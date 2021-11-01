966c6e303a8d71d0300caa10510c3da314d6cc94
YMatoi <YMatoi@users.noreply.github.com>
2021-11-01 22:21:20 +0900

    add Timber

--- app/src/main/java/com/github/ymatoi/wifiscanner/ui/screen/MainScreen.kt
+++ app/src/main/java/com/github/ymatoi/wifiscanner/ui/screen/MainScreen.kt
@@ -5,11 +5,16 @@ import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
     val message = "Hello, World"
+
+    init {
+        Timber.d("Initialize MainViewModel")
+    }
 }
 
 @Composable
