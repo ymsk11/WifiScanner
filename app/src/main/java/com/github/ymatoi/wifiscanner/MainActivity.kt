579a64cba51f01fcdf16e3717a206ad6892e3d32
YMatoi <YMatoi@users.noreply.github.com>
2021-11-01 21:53:22 +0900

    add ktlint

--- app/src/main/java/com/github/ymatoi/wifiscanner/MainActivity.kt
+++ app/src/main/java/com/github/ymatoi/wifiscanner/MainActivity.kt
@@ -5,13 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.ymatoi.wifiscanner.ui.screen.MainScreen
 import com.github.ymatoi.wifiscanner.ui.theme.WifiScannerTheme
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
