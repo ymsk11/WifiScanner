3544550c742297ec6799812837b16e7566e3e40a
YMatoi <105350536+ymsk11@users.noreply.github.com>
2022-12-29 16:02:22 +0900

    add topappbar scroll behavior

--- app/src/main/java/com/github/ymatoi/wifiscanner/ui/screen/MainScreen.kt
+++ app/src/main/java/com/github/ymatoi/wifiscanner/ui/screen/MainScreen.kt
@@ -16,11 +16,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -85,19 +87,24 @@ fun MainScreen(
     onClickConfirmButton: () -> Unit
 ) {
     val state = rememberPullRefreshState(refreshing = isRefreshing, onRefresh = onRefresh)
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Scaffold(
         topBar = {
-            TopAppBar(title = {
-                TextField(
-                    value = searchKeyword,
-                    onValueChange = onValueSearchKeywordChange,
-                    modifier = Modifier
-                        .padding(start = 8.dp, end = 8.dp, top = 8.dp)
-                        .fillMaxWidth()
-                )
-            })
-        }
+            TopAppBar(
+                title = {
+                    TextField(
+                        value = searchKeyword,
+                        onValueChange = onValueSearchKeywordChange,
+                        modifier = Modifier
+                            .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+                            .fillMaxWidth()
+                    )
+                },
+                scrollBehavior = scrollBehavior
+            )
+        },
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     ) {
         Box(
             modifier = Modifier
